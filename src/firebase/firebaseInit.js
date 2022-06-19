25ff5f72c41bfc8bac9899c5239b97ad3338c246
Sicelo Sitsha <70627871+sicelo17@users.noreply.github.com>
2022-06-19 14:25:03 +0200

    added new firebase app

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,14 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAvlpkV5FLM63pBNDtMtoQ3ecxv3RyrMck",
     authDomain: "invoice-app-proj.firebaseapp.com",
     projectId: "invoice-app-proj",
     storageBucket: "invoice-app-proj.appspot.com",
     messagingSenderId: "521044525535",
-    appId: "1:521044525535:web:507c3c061e5ff6b931846a"
-};
+    appId: "1:521044525535:web:00f6445871e5c95e31846a"
+  };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
